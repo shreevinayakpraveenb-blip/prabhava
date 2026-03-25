ad52c1ffb1074e09c85f9e6e2aa293c7aa8ea2b2
shreevinayakpraveenb-blip <shreevinayakpraveenb@gmail.com>
2026-03-25 06:05:03 +0530

    Update Supabase anon key in auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://pkgaxclfrhaguzbckfdx.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_o9oXYMlV59VSZ_rznHdDKg_64yVVY1F';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZ2F4Y2xmcmhhZ3V6YmNrZmR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDg1NTMsImV4cCI6MjA4OTA4NDU1M30.zrKUzOJOxbyWN9CtNuMD14ZKmNz8jRiW3AkTwOoYVFU';
 
 const { createClient } = window.supabase;
 const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
